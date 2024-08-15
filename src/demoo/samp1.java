a10287c33cc32b00f9b65ad6cf934c7a5921ee37
User <User@DESKTOP-QV8R97I.mshome.net>
2024-08-15 15:16:13 +0530

    commited

--- src/demoo/samp1.java
+++ src/demoo/samp1.java
@@ -1,8 +1,12 @@
 package demoo;
 
+import org.testng.annotations.Test;
+
 public class samp1 
 {
-	public static void main(String[] args) {
+	@Test
+	public void abb()
+	{
 		System.out.println("Hello Java");
 	}
 
